b19d9cf2a9b536b200607e85ededf175d53d0219
Jordan Rey <rey.jordan.1998@gmail.com>
2022-10-23 21:02:13 +0200

    undo module-info.java fix, fix surefire opens instead

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,11 @@
-open module com.arenareturns.simplenet {
+module com.arenareturns.simplenet {
     requires org.slf4j;
     requires com.github.pbbl;
+
+    exports com.arenareturns.simplenet;
+    exports com.arenareturns.simplenet.packet;
+    exports com.arenareturns.simplenet.utility.exposed.consumer;
+    exports com.arenareturns.simplenet.utility.exposed.cryptography;
+    exports com.arenareturns.simplenet.utility.exposed.data;
+    exports com.arenareturns.simplenet.utility.exposed.predicate;
 }
